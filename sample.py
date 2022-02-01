881f9e5299c43e49a2e3d4478dcb2b7b06c242c3
maliabarker <89502882+maliabarker@users.noreply.github.com>
2022-01-31 16:12:55 -1000

    organized files

--- sample.py
+++ sample.py
@@ -6,16 +6,16 @@ def random_word(histogram_list):
     word = tuple[0]
     return word
     
-def random_word_weighted(histogram):
-    words = list()
-    for key in histogram:
-        i = int(histogram[key])
-        for x in range(0, i):
-            words.append(key)
-    rand_num = random.randint(0, len(words) - 1)
-    return words[rand_num]
+# def random_word_weighted(histogram):
+#     words = list()
+#     for key in histogram:
+#         i = int(histogram[key])
+#         for x in range(0, i):
+#             words.append(key)
+#     rand_num = random.randint(0, len(words) - 1)
+#     return words[rand_num]
 
-def test(histogram):
+def weighted_word(histogram):
     distance = 0
     dart = random.randint(0, sum(histogram.values()))
     for key, value in histogram.items():
@@ -31,6 +31,5 @@ if __name__ == '__main__':
     # list = list(iterable)
     # print(list)
     # print(random_word(list))
-    print(random_word_weighted(histogram))
-    print(test(histogram))
-    print(string.punctuation)
+    # print(random_word_weighted(histogram))
+    print(weighted_word(histogram))
